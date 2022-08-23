cdfac651adf2034ebab29253cca893f73846b12a
Zhuravlev Dmitry <qwerty-91@list.ru>
2022-08-23 19:51:10 +0500

    Initial Commit

---
+++ BrokeBoys FC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrokeBoys FC
+//
+//  Created by Zhuravlev Dmitry on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
